5063b7d6346b3446521481c76085816a67bf815d
bimaatriadmaja <bimaatriadmaja17@gmail.com>
2024-05-14 13:50:56 +0700

    react redux

---
+++ redux-group3/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CounterComponent from "./components/CounterComponent";
+import UserComponent from "./components/UserComponent";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Redux App Group 3 - Menyalaa🔥</h1>
+      <CounterComponent />
+      <UserComponent />
+    </div>
+  );
+}
+
+export default App;
